7f968fd79ceebcb27d7d3556020e78da35ee8eea
Mahdi Abbariki <mahdi.abbariki0@gmail.com>
2024-07-09 15:18:09 +0330

    feat (User Credit History): added an interface for changing user credit history commands
    added an interface for classes that deals with user credit history. any kind of changes in user credit must be done using this interface.

---
+++ app/UserCreditHistory/UserCreditHistoryCommandInterface.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\UserCreditHistory;
+
+use App\Models\User;
+use App\Models\UserCreditHistory;
+use Illuminate\Database\Eloquent\Model;
+
+interface UserCreditHistoryCommandInterface
+{
+    public static function getTypeName(): string;
+    
+    public static function execute(User $user, int $amount, string $description = null): UserCreditHistory;
+
+    public static function revert(User $user, int $amount, string $description = null): UserCreditHistory;
+}
