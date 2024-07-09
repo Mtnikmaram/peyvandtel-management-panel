61de2aa373448e7fbcc3ec7067409d30d8e0c783
Mahdi Abbariki <mahdi.abbariki0@gmail.com>
2024-07-09 17:42:14 +0330

    feat (Json Cast): added flags to default encoder
    added needed flags for UTF-8 support, to the default encoder of json that encodes/decodes the json in the models.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Casts\Json;
 use Illuminate\Http\Response as HttpResponse;
 use Illuminate\Support\Facades\Response;
 use Illuminate\Support\ServiceProvider;
@@ -24,5 +25,8 @@ class AppServiceProvider extends ServiceProvider
         Response::macro('apiError', function (string $name, string $reason) {
             return Response::json(['errors' => [$name ?? 'error' => [$reason]]], HttpResponse::HTTP_UNPROCESSABLE_ENTITY);
         });
+
+        Json::encodeUsing(fn ($value) => json_encode(value: $value, flags: JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK));
+        Json::decodeUsing(fn ($value, $associative) => json_decode(json: $value, associative: $associative, flags: JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES | JSON_NUMERIC_CHECK));
     }
 }
